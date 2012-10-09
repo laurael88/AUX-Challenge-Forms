854d433c4c77df95d18f31f8581962abdffca96c
Laura Lozano <laura.elena.lozano@gmail.com>
2012-10-09 13:20:52 -0400

    Made minor adjustments to the HTML and finished styling the form with CSS

--- build/index.php
+++ build/index.php
@@ -2,7 +2,10 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title></title>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Kreon:400,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+    <title>AUX Forms Challenge</title>
 </head>
 
 <body>
@@ -48,81 +51,98 @@
 
             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
                 
-                <article id="portfolio_info">
+                <fieldset id="portfolio_info">
                     <div id="number_1">
                         1
                     </div>
-                    <h1>First, name your portfolio</h1>
+                    <legend>First, name your portfolio</legend>
+                    <div class="clear"></div> <!--clear-->
 
-                    <label for="portfolio">Portfolio title</label>
-                    <input type="text" id="portfolio" name="portfolio" minlength="2"/>
+                    <div class="gray_container">
+                        <label for="portfolio">Portfolio title</label>
+                        <input type="text" id="portfolio" name="portfolio" minlength="2"/>
 
-                    <label for="website">Portfolio address</label>
-                    <input type="text" id="website" name="website" minlength="2"/>
-                </article> <!--portfolio_info-->
+                        <label for="website">Portfolio address</label>
+                        <input type="text" id="website" name="website" minlength="2"/>
+                    </div> <!--gray_container-->
+                </fieldset> <!--portfolio_info-->
 
-                <article id="account_details">
+                <fieldset id="account_details">
                     <div id="number_2">
                         2
                     </div>
-                    <h1>Now, enter your account details</h1>
+                    <legend>Now, enter your account details</legend>
+                    <div class="clear"></div> <!--clear-->
 
-                    <label for="name">Name</label>
-                    <input type="text" id="name" name="name" minlength="2"/>
-                    
-                    <label for="name">E-mail</label>
-                    <input type="email" id="email" name="email" minlength="2"/>
+                    <div class="gray_container">
+                        <label for="name">Name</label>
+                        <input type="text" id="name" name="name" minlength="2"/>
+                        
+                        <label for="name">E-mail</label>
+                        <input type="email" id="email" name="email" minlength="2"/>
+                        <aside>NOTE: We'll never share your email, promise.</aside>
 
-                    <label for="user_password">Password</label>
-                    <input type="password" id="user_password" name="user_password" minlength="6"/>
-                </article> <!--portfolio_info-->
+                        <label for="user_password">Password</label>
+                        <input type="password" id="user_password" name="user_password" minlength="6"/>
+                    </div> <!--gray_container-->
+                </fieldset> <!--portfolio_info-->
                 
-                <article id="payment_info">
+                <fieldset id="payment_info">
                     <div id="number_3">
                         3
                     </div>
-                    <h1>Finally, enter your payment information</h1>
+                    <legend>Finally, enter your payment information</legend>
+                    <div class="clear"></div> <!--clear-->
 
-                    <label for="credit_card">Card number</label>
-                    <input type="text" id="credit_card" name="credit_card"/>
+                    <div class="gray_container">
+                        <label for="credit_card">Card number</label>
+                        <input type="text" id="credit_card" name="credit_card"/>
 
-                    <div id="credit_card_logos">
-                    </div> <!--credit_card_logos-->
+                        <div id="credit_card_logos">
+                        </div> <!--credit_card_logos-->
 
-                    <label for="security_code">Security code</label>
-                    <input type="text" id="security_code" name="security_code" minlength="3"/>
+                        <div class="clear"></div> <!--clear-->
 
-                    <label for="expiration">Expiration date</label>
-                    <select name="expiration" id="expiration">
-                        <option>Month</option>
-                        <option>December</option>
-                        <option>November</option>
-                        <option>October</option>
-                        <option>September</option>
-                        <option>August</option>
-                        <option>July</option>
-                        <option>June</option>
-                        <option>May</option>
-                        <option>April</option>
-                        <option>March</option>
-                        <option>February</option>
-                        <option>January</option>
-                    </select>
-                    <select name="year" id="year">
-                        <option>Year</option>
-                        <option>2020</option>
-                        <option>2019</option>
-                        <option>2018</option>
-                        <option>2017</option>
-                        <option>2016</option>
-                        <option>2015</option>
-                        <option>2014</option>
-                        <option>2013</option>
-                        <option>2012</option>
-                    </select>
+                        <label for="security_code">Security code</label>
+                        <input type="text" id="security_code" name="security_code" minlength="3"/>
 
-                    <div id="locked"></div><!--locked-->
-                </article> <!--payment_info-->
+                        <div id="security_img">
+                        </div> <!--credit_card_logos-->
+
+                        <div class="clear"></div> <!--clear-->
+
+                        <label for="expiration">Expiration date</label>
+                        <select name="expiration" id="expiration">
+                            <option>Month</option>
+                            <option>December</option>
+                            <option>November</option>
+                            <option>October</option>
+                            <option>September</option>
+                            <option>August</option>
+                            <option>July</option>
+                            <option>June</option>
+                            <option>May</option>
+                            <option>April</option>
+                            <option>March</option>
+                            <option>February</option>
+                            <option>January</option>
+                        </select>
+                        <select name="year" id="year">
+                            <option>Year</option>
+                            <option>2020</option>
+                            <option>2019</option>
+                            <option>2018</option>
+                            <option>2017</option>
+                            <option>2016</option>
+                            <option>2015</option>
+                            <option>2014</option>
+                            <option>2013</option>
+                            <option>2012</option>
+                        </select>
+                        
+                        <div id="locked"></div><!--locked-->
+                    </div> <!--gray_container-->
+                </fieldset> <!--payment_info-->
 
                 <button type="submit" name="send">Create your portfolio</button>
                 
